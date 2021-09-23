bf27cde4b5a89427c1bef48ef1b85f072aaf0677
jmcarriedo <87625886+jmcarriedo@users.noreply.github.com>
2021-09-23 21:41:20 +0800

    update

--- user/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    $_SESSION['status'] = 'invalid';
-    unset($_SESSION['email']);
-    unset($_SESSION['userid']);
-    unset($_SESSION['accessRole']);
-    echo "<script> window.location.href='../dashboard.php' </script>";
-?>
--- user/session.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if($_SESSION['status'] == 'invalid' || empty($_SESSION['status'])) {
-        $_SESSION['status'] = 'invalid';
-        unset($_SESSION['email']);
-        header('location: login.php');
-    }
-?>
